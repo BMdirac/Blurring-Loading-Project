4fbe2e6c68aff1021e97328dde967cdcddbd7151
BMdirac <busarimoruf20@gmail.com>
2022-07-22 22:45:12 +0900

    new changes

--- script.js
+++ script.js
@@ -12,13 +12,13 @@ function blurring() {
         clearInterval(int)
     }
 
-    loadText.innerText = `${load}%`
+    loadText.innerText = `${load}%` // this line code from top makes the text load from 0 to 100 ( note that innerHTML can also be use in replace of innerText)
     loadText.style.opacity = scale(load, 0, 100, 1, 0)
     // background image is going to go from a blur of 30px to 0px in the same amount of time that the load goes from 0 to 100%
     bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)` 
 }
 
-// stackoverflow.com
+// stackoverflow.com ( note that this function scale should not be placed inside the blurring function)
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
